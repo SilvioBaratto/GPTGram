6b52ddf5ba24e52033ef870c285ea4958ea7a050
Silvio Angelo Baratto Roldan <30952948+SilvioBaratto@users.noreply.github.com>
2023-07-09 10:43:52 +0200

    Add files via upload

--- GPTGram/model/_gpt.py
+++ GPTGram/model/_gpt.py
@@ -2,7 +2,6 @@ import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import _transformer
 from ._transformer import LayerNorm, Block
 from ..config import Config as cfg
 
